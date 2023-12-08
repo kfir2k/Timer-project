48da68580112110f5a74812e2e805c35751f8033
kfir <kfir.kotler@gmail.com>
2023-12-08 23:00:20 +0200

    more vishuls

--- clock.js
+++ clock.js
@@ -1,14 +1,14 @@
 const clock = document.getElementById("clock");
 const startStop = document.getElementById("start-stop");
 const clearRound = document.getElementById("clear-round");
-const start = document.querySelector(".start")
-const stopc = document.querySelector(".stop")
-const clear = document.querySelector(".clear")
-const round = document.querySelector(".round")
-const roundList = document.getElementById("rounds")
+const start = document.querySelector(".start");
+const stopc = document.querySelector(".stop");
+const clear = document.querySelector(".clear");
+const round = document.querySelector(".round");
+const roundList = document.getElementById("rounds");
 
-let stateBtn1 = false
-let stateBtn2 = false
+let stateBtn1 = false;
+let stateBtn2 = false;
 
 start.innerHTML = "start";
 clear.innerHTML = "clear";
@@ -29,14 +29,14 @@ let goClock = () => {
             start.innerHTML = "start";
         }
         clock.innerHTML = `${count.min.toString().padStart(2, '0')}:${count.sec.toString().padStart(2, '0')}:${count.ms.toString().padStart(2, '0')}`
-        count.ms++
+        count.ms++;
         console.log(count);
         if (count.ms === 100) {
             count.ms = 0;
-            count.sec++
+            count.sec++;
             if (count.sec === 60) {
                 count.sec = 0;
-                count.min++
+                count.min++;
             }
         }
     }, 10);
@@ -45,54 +45,44 @@ let goClock = () => {
 
 let indexOfRound = 0
 let printRound = () => {
-
-    indexOfRound++
+    indexOfRound++;
     roundList.innerHTML += `<div/>${indexOfRound}. ${count.min.toString().padStart(2, '0')}:${count.sec.toString().padStart(2, '0')}:${count.ms.toString().padStart(2, '0')}<div>`
-
 }
-
-
-
 function btn1() {
     console.log("btn1", stateBtn1);
     if (stateBtn1 === false) {
-        stateBtn1 = true
-        stateBtn2 = true
-        round.innerHTML = "round"
+        stateBtn1 = true;
+        stateBtn2 = true;
+        round.innerHTML = "round";
         goClock()
     } else {
-        stateBtn1 = false
-        stateBtn2 = false
-        clear.innerHTML = "clear"
-
+        stateBtn1 = false;
+        stateBtn2 = false;
+        clear.innerHTML = "clear";
     }
-
 }
-
 
 function btn2() {
     console.log("btn2", stateBtn2);
     if (stateBtn2 === false && stateBtn1 === false) {
         clearedCounter()
     } else {
-        round.innerHTML = "round"
+        round.innerHTML = "round";
         printRound()
     }
 }
 
-
-
 let clearedCounter = () => {
-    clock.innerHTML = "00:00:00"
-    count.ms = 0
-    count.min = 0
-    count.sec = 0
-    roundList.innerHTML = ""
-    indexOfRound = 0
+    clock.innerHTML = "00:00:00";
+    count.ms = 0;
+    count.min = 0;
+    count.sec = 0;
+    roundList.innerHTML = "";
+    indexOfRound = 0;
 }
 window.addEventListener("load", clearedCounter);
-startStop.addEventListener("click", btn1)
-clearRound.addEventListener("click", btn2)
+startStop.addEventListener("click", btn1);
+clearRound.addEventListener("click", btn2);
 
 
 
